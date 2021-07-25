0e386d4de06daa9f1acfa18ea2da4ebf206a9fb0
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-07-25 08:14:25 +0200

    basic tree structure and first test

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,32 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+const Slot = [32]u8;
+const Key = [32]u8;
+const Stem = [31]u8;
+const Hash = [32]u8;
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+const Node = union(enum) {
+    last_level: struct {
+        values: Slot,
+        key: Stem,
+    },
+    branch: struct {
+        children: [256]?*Node,
+        depth: u8,
+        count: u8,
+    },
+    hash: Hash,
+
+    fn insert(self: Node, key: Key, value: Slot) !void {
+        return switch (self) {
+            .hash => error.InsertIntoHash,
+            else => error.NodeTypeNotSupported,
+        };
+    }
+};
+
+test "inserting into hash raises an error" {
+    var root = Node{ .hash = [_]u8{0} ** 32 };
+    try testing.expectError(error.InsertIntoHash, root.insert([_]u8{0} ** 32, [_]u8{0} ** 32));
 }
