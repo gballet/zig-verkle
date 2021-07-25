d68fb51977b83e401df774fa485cf2cc0248060b
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-07-25 11:47:23 +0200

    fix memory leak

--- src/main.zig
+++ src/main.zig
@@ -26,25 +26,21 @@ const Node = union(enum) {
         return @This(){ .empty = null };
     }
 
-    fn destroy(self: *Node, allocator: *Allocator) void {
-        switch (self.*) {
-            else => {}, // unsupported node type
-        }
-    }
-
     fn insert(self: *Node, key: Key, value: *Slot, allocator: *Allocator) !void {
         return switch (self.*) {
             .hash => error.InsertIntoHash,
-            .empty => |*llo| {
+            .empty => {
                 const slot = key[31];
-                var ll = try allocator.create(LastLevelNode);
-                errdefer allocator.destroy(ll);
+                var ll = LastLevelNode{
+                    .values = [_]?*Slot{null} ** 256,
+                    .key = [_]u8{0} ** 31,
+                };
                 // Do not copy the value for now. This might cause some
                 // heisenbugs in the future, as immutability isn't enforced.
                 // It will do for now.
                 ll.values[slot] = value;
                 std.mem.copy(u8, ll.key[0..], key[0..31]);
-                self.* = @unionInit(Node, "last_level", ll.*);
+                self.* = @unionInit(Node, "last_level", ll);
             },
             else => error.NodeTypeNotSupported,
         };
@@ -59,12 +55,19 @@ test "inserting into hash raises an error" {
 
 test "insert into empty tree" {
     var root = Node.new();
-    defer root.destroy(testing.allocator);
     var value = [_]u8{0} ** 32;
     try root.insert([_]u8{0} ** 32, &value, testing.allocator);
 
     switch (root) {
-        Node.last_level => {},
+        Node.last_level => |*ll| {
+            for (ll.values) |v, i| {
+                if (i == 0) {
+                    try testing.expect(v != null);
+                } else {
+                    try testing.expect(v == null);
+                }
+            }
+        },
         else => return error.InvalidNodeType,
     }
 }
