bd898df31bdc4184c2b4615a6408932cb18b6a08
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-12-13 12:54:08 +0100

    support for insertion in extension node

--- src/main.zig
+++ src/main.zig
@@ -13,7 +13,7 @@ const LastLevelNode = struct {
 };
 
 const BranchNode = struct {
-    children: [256]?*Node,
+    children: [256]?Node,
     depth: u8,
     count: u8,
 };
@@ -30,6 +30,10 @@ const Node = union(enum) {
     }
 
     fn insert(self: *Node, key: Key, value: *Slot, allocator: *Allocator) !void {
+        return self.insert_with_depth(key, value, allocator, 0);
+    }
+
+    fn insert_with_depth(self: *Node, key: Key, value: *Slot, allocator: *Allocator, depth: u8) !void {
         return switch (self.*) {
             .empty => {
                 const slot = key[31];
@@ -45,12 +49,29 @@ const Node = union(enum) {
             },
             .hash => error.InsertIntoHash,
             .last_level => |ll| {
+                // Check if the keys are the same, if so, then just place the value
+                // in the corresponding slot, as the final extension tree has been
+                // reached.
                 const diffidx = std.mem.indexOfDiff(u8, ll.key[0..], key[0..31]);
+                if (diffidx == null) {
+                    ll.values[key[31]] = value;
+                    return;
+                }
+
                 var br = try allocator.create(BranchNode);
-                br.children = [_]?*Node{null} ** 256;
-                br.depth = 0;
+                br.children = [_]?Node{null} ** 256;
+                br.depth = depth;
                 br.count = 2;
+                br.children[ll.key[br.depth]] = Node{ .last_level = ll };
+                if (ll.key[br.depth] != key[br.depth]) {
+                    var ll2 = try allocator.create(LastLevelNode);
+                    ll2.key = [_]u8{0} ** 31;
+                    std.mem.copy(u8, ll2.key[0..], key[0..31]);
+                    ll2.values = [_]?*Slot{null} ** 256;
+                    br.children[key[br.depth]] = Node{ .last_level = ll2 };
+                }
                 self.* = @unionInit(Node, "branch", br);
+                return br.children[key[br.depth]].?.insert_with_depth(key, value, allocator, depth + 1);
             },
             else => error.NodeTypeNotSupported,
         };
@@ -69,6 +90,9 @@ const Node = union(enum) {
                 //}
 
                 allocator.destroy(ll);
+            },
+            .branch => |br| {
+                allocator.destroy(br);
             },
             // TODO complete list
             else => @panic("unsupported node type in tear_down"),
@@ -94,6 +118,28 @@ test "insert into empty tree" {
         Node.last_level => |ll| {
             for (ll.values) |v, i| {
                 if (i == 0) {
+                    try testing.expect(v != null);
+                } else {
+                    try testing.expect(v == null);
+                }
+            }
+        },
+        else => return error.InvalidNodeType,
+    }
+}
+
+test "insert into a last_level node, difference in suffix" {
+    var root_ = Node.new();
+    var root = &root_;
+    var value = [_]u8{0} ** 32;
+    try root.insert([_]u8{0} ** 32, &value, testing.allocator);
+    try root.insert([_]u8{0} ** 31 ++ [1]u8{1}, &value, testing.allocator);
+    defer root.tear_down(testing.allocator);
+
+    switch (root.*) {
+        Node.last_level => |ll| {
+            for (ll.values) |v, i| {
+                if (i < 2) {
                     try testing.expect(v != null);
                 } else {
                     try testing.expect(v == null);
