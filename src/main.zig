675afcc28f4ecfa3e295d56062e2b60fe7f86fb1
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2023-11-27 12:54:15 +0100

    implement toDot for branch nodes

--- src/main.zig
+++ src/main.zig
@@ -255,6 +255,23 @@ const Node = union(enum) {
             _ => error.NotImplemented,
         };
     }
+    fn toDot(self: *const Node, allocator: Allocator, path: []const u8, parent: []const u8) ![]const u8 {
+        const hash = self.commitment().mapToScalarField();
+        const me = std.fmt.allocPrint(allocator, "internal{s}", .{path});
+        var sofar: []u8 = std.fmt.allocPrint(allocator, "{}\n{} [label=\"I: {}\"]", .{ me, hash });
+        if (parent.len > 0) {
+            sofar = std.fmt.allocPrint(allocator, "{} {}", .{ sofar, me });
+        }
+        switch (self.*) {
+            .branch => |br| for (br.children, 0..) |child, childidx| {
+                const child_path = std.fmt.allocPrint(allocator, "{}{}", .{ me, childidx });
+                sofar = std.fmt.allocPrint(allocator, "{}\n{}", .{ sofar, child.toDot(allocator, child_path, me) });
+            },
+            _ => {}, // ignore other node types for now
+        }
+
+        return std.fmt.allocPrint(allocator, "digraph {\n {}\n}", .{sofar});
+    }
 };
 
 test "inserting into hash raises an error" {
